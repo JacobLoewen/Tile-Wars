045703db76d42d9325237478e7b1d88a89b705aa
Jacob Loewen <jakeploewen@gmail.com>
2024-01-09 09:57:12 -0800

    Pulling

---
+++ test.py
@@ -0,0 +1,13 @@
+# listy = [[1, 2]]
+
+# listy.append([4])
+# listy[1].append(5)
+
+# print(listy)
+
+notes_list = ["yo", "mama"]
+index = [0, 1]
+
+note = lambda: [notes_list[index[0]], notes_list[index[1]]]
+
+print(note())
